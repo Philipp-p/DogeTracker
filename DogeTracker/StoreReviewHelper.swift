1d47f6bc2b442889696bca6b32391cc551d4ccf4
Philipp-p <Philipp-p@users.noreply.github.com>
2018-12-17 20:14:16 +0100

    correctly adapted to swift 4.2

--- DogeTracker/StoreReviewHelper.swift
+++ DogeTracker/StoreReviewHelper.swift
@@ -83,14 +83,9 @@ class StoreReviewHelper {
     fileprivate static func openUrl(_ urlString:String) {
         let url = URL(string: urlString)!
         if #available(iOS 10.0, *) {
-            UIApplication.shared.open(url, options: convertToUIApplicationOpenExternalURLOptionsKeyDictionary([:]), completionHandler: nil)
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
         } else {
             UIApplication.shared.openURL(url)
         }
     }
-}
-
-// Helper function inserted by Swift 4.2 migrator.
-fileprivate func convertToUIApplicationOpenExternalURLOptionsKeyDictionary(_ input: [String: Any]) -> [UIApplication.OpenExternalURLOptionsKey: Any] {
-	return Dictionary(uniqueKeysWithValues: input.map { key, value in (UIApplication.OpenExternalURLOptionsKey(rawValue: key), value)})
 }
